d51569b7b6aaf5ae20bbd0b7f0174d2fc7d5a214
Ujef Rashid Jamadar <138428159+ujefjamadar@users.noreply.github.com>
2024-05-25 17:49:07 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,136 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").css("top", "0px");
+    } else {
+      $(".sticky-top").css("top", "-100px");
+    }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
+
+function changeReadMore() {
+  const mycontent = document.getElementById("mybox1id");
+  const mybutton = document.getElementById("mybuttonid");
+  const span1 = document.getElementById("span1");
+
+  if (mycontent.style.display === "none" || mycontent.style.display === "") {
+    mycontent.style.display = "inline";
+    span1.style.display = "none";
+    mybutton.textContent = "Read Less";
+  } else {
+    mycontent.style.display = "none";
+    mybutton.textContent = "Read More";
+    span1.style.display = "inline";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var readMoreLinks = document.querySelectorAll(".read-more");
+    
+    readMoreLinks.forEach(function(link) {
+        link.addEventListener("click", function() {
+            var moreText = this.nextElementSibling;
+            
+            if (moreText.style.display === "none") {
+                moreText.style.display = "block";
+                this.textContent = "Read Less";
+            } else {
+                moreText.style.display = "none";
+                this.textContent = "Read More";
+            }
+        });
+    });
+});
+// </script>
